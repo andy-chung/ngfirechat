a2031c61766ebfaf0d9819967ba85acc6d11b47c
andy-chung <andy.chung.0@gmail.com>
2020-10-23 01:11:31 +0800

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyA8aI0QTyllxRMSiEjILYRYW-pLBUYxF64",
+    authDomain: "friendlychatproject-a98f0.firebaseapp.com",
+    databaseURL: "https://friendlychatproject-a98f0.firebaseio.com",
+    projectId: "friendlychatproject-a98f0",
+    storageBucket: "friendlychatproject-a98f0.appspot.com",
+    messagingSenderId: "413106324043",
+    appId: "1:413106324043:web:b1031aeeecdb99735743ce",
+    //measurementId: "G-KLBLGJESH3"
+    measurementId: "G-0BN70ZP3LH"
+  }  
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,38 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyA8aI0QTyllxRMSiEjILYRYW-pLBUYxF64",
+    authDomain: "friendlychatproject-a98f0.firebaseapp.com",
+    databaseURL: "https://friendlychatproject-a98f0.firebaseio.com",
+    projectId: "friendlychatproject-a98f0",
+    storageBucket: "friendlychatproject-a98f0.appspot.com",
+    messagingSenderId: "413106324043",
+    appId: "1:413106324043:web:b1031aeeecdb99735743ce",
+    //measurementId: "G-KLBLGJESH3"
+    measurementId: "G-0BN70ZP3LH"
+
+  /**** web friendlychat
+    apiKey: "AIzaSyBcrducmeC7sVl6C_RAuRl1r5dPL1KY0yo",
+    authDomain: "ngfirechat-project.firebaseapp.com",
+    databaseURL: "https://ngfirechat-project.firebaseio.com",
+    projectId: "ngfirechat-project",
+    storageBucket: "ngfirechat-project.appspot.com",
+    messagingSenderId: "402022006130",
+    appId: "1:402022006130:web:b28b1b722033e9462b71eb"
+  ****/
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
