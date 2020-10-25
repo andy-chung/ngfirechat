b8fc43569c3a17c44d83d608802a73ca881978a2
andy-chung <andy.chung.0@gmail.com>
2020-10-25 11:31:43 +0800

    mask connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,15 +2,14 @@ export const environment = {
   production: true,
 
   firebase: {
-    apiKey: "AIzaSyA8aI0QTyllxRMSiEjILYRYW-pLBUYxF64",
-    authDomain: "friendlychatproject-a98f0.firebaseapp.com",
-    databaseURL: "https://friendlychatproject-a98f0.firebaseio.com",
-    projectId: "friendlychatproject-a98f0",
-    storageBucket: "friendlychatproject-a98f0.appspot.com",
-    messagingSenderId: "413106324043",
-    appId: "1:413106324043:web:b1031aeeecdb99735743ce",
-    //measurementId: "G-KLBLGJESH3"
-    measurementId: "G-0BN70ZP3LH"
+    apiKey: "your-firebase-config",
+    authDomain: "your-firebase-config",
+    databaseURL: "your-firebase-config",
+    projectId: "your-firebase-config"
+    storageBucket: "your-firebase-config",
+    messagingSenderId: "your-firebase-config",
+    appId: "your-firebase-config",
+    measurementId: "your-firebase-config"
   }  
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,24 +6,16 @@ export const environment = {
   production: false,
 
   firebase: {
-    apiKey: "AIzaSyA8aI0QTyllxRMSiEjILYRYW-pLBUYxF64",
-    authDomain: "friendlychatproject-a98f0.firebaseapp.com",
-    databaseURL: "https://friendlychatproject-a98f0.firebaseio.com",
-    projectId: "friendlychatproject-a98f0",
-    storageBucket: "friendlychatproject-a98f0.appspot.com",
-    messagingSenderId: "413106324043",
-    appId: "1:413106324043:web:b1031aeeecdb99735743ce",
-    //measurementId: "G-KLBLGJESH3"
-    measurementId: "G-0BN70ZP3LH"
+    apiKey: "your-firebase-config",
+    authDomain: "your-firebase-config",
+    databaseURL: "your-firebase-config",
+    projectId: "your-firebase-config"
+    storageBucket: "your-firebase-config",
+    messagingSenderId: "your-firebase-config",
+    appId: "your-firebase-config",
+    measurementId: "your-firebase-config"
 
   /**** web friendlychat
-    apiKey: "AIzaSyBcrducmeC7sVl6C_RAuRl1r5dPL1KY0yo",
-    authDomain: "ngfirechat-project.firebaseapp.com",
-    databaseURL: "https://ngfirechat-project.firebaseio.com",
-    projectId: "ngfirechat-project",
-    storageBucket: "ngfirechat-project.appspot.com",
-    messagingSenderId: "402022006130",
-    appId: "1:402022006130:web:b28b1b722033e9462b71eb"
   ****/
   }
 };
